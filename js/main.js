36fc8d66b072668b56fc27f3bda5573915d4dba9
Winter Weng <yichinweng@gmail.com>
2022-12-01 22:50:19 +0800

    feat: update ga.

--- js/main.js
+++ js/main.js
@@ -149,15 +149,4 @@ $(function () {
       }
     }
   });
-
-  // GA
-  (function (b, o, i, l, e, r) {
-    b.GoogleAnalyticsObject = l; b[l] || (b[l] =
-        function () { (b[l].q = b[l].q || []).push(arguments) }); b[l].l = +new Date;
-        e = o.createElement(i); r = o.getElementsByTagName(i)[0];
-        e.src = 'https://www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e, r)
-    }(window, document, 'script', 'ga'));
-  ga('create', 'UA-65425974-1', 'auto'); ga('send', 'pageview');
-
 });
